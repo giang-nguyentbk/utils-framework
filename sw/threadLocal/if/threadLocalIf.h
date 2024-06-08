9040c28e0ae4917ec675ec0904014ea2701e2d35
Giang Nguyen <giang.nguyentbk@gmail.com>
2024-06-08 18:38:14 +0700

    NOREF: Adapt utils-framework to SDK
    Team: Caliburn
    giangnguyentbk

--- sw/threadLocal/if/threadLocalIf.h
+++ sw/threadLocal/if/threadLocalIf.h
@@ -29,39 +29,26 @@ namespace implementation /* private namespace, do not use directly the classes i
 *
 * By implementation based on pthread get/set_specific methods, we can allocate blocks of data which can only
 * be accessed by the current local thread via associated unique tlsKeys. */
-template <class T, int instanceNum = 1>
+template <class T>
 class ThreadLocalLinuxImpl
 {
 public:
-    /*! @brief End users only need to know that this method is to allocate a new thread local data which has type of T.
-    *   @return Return an address of the thread-specific data object (of type T) which is allocated in the heap and
-    * is uniquely associated with its m_tlsKey. */
     static T& get()
-    {
-        static DefaultAllocator defaultAllocator;
-        return get(defaultAllocator);
-    }
-
-    /*! @brief Creates the tlsKey and allocates T's thread-local object if not exists.
-    *   @return Return an address of the thread-specific data object (of type T) which is allocated in the heap and
-    * is uniquely associated with its m_tlsKey. */
-    template <class Allocator>
-    static T& get(Allocator& create)
     {
         createKey();
         T* instance = (T*)::pthread_getspecific(m_tlsKey);
         if(!instance)
         {
-            instance = create();
+            instance = new T;
             assert(instance);
             ::pthread_setspecific(m_tlsKey, instance);
         }
         return *instance;
     }
 
-    /*! @brief You also can get a pointer to the thread-local data object instead through getPtr().
-    *   @return Return a pointer to the thread-specific data object (of type T) which was allocated in the heap and
-    * was uniquely associated with its m_tlsKey. Return NULL if get() request hasn't been initilized. */
+    /* 	You also can get a pointer to the thread-local data object instead through getPtr().
+    *   Return a pointer to the thread-specific data object (of type T) which was allocated in the heap and
+    * 	was uniquely associated with its m_tlsKey. Return NULL if get() request hasn't been initilized. */
     static T* getPtr()
     {
         createKey();
@@ -81,9 +68,9 @@ public:
     }
 
 private:
-    /*! @brief If key not created yet, create a TLS key with pthread_once() and pthread_key_create().
-    * Key is created once and only once (only the first time).
-    * Subsequent calls of createKey() will not run tlsMakeKey anymore. */
+    /*	If key not created yet, create a TLS key with pthread_once() and pthread_key_create().
+    * 	Key is created once and only once (only the first time).
+    * 	Subsequent calls of createKey() will not run tlsMakeKey anymore. */
     static void createKey()
     {
         if(!m_isKeyCreated)
@@ -93,27 +80,15 @@ private:
         }
     }
 
-    /*! @brief Define how T will be created by default. DefaultAllocator will use "new" operator to allocate T's object
-    * in the heap memory.
-    *   @return Return a pointer to allocated memory with type of T. */
-    class DefaultAllocator
-    {
-    public:
-        T* operator()()
-        {
-            return new T;
-        }
-    };
-
-    /*! @brief Create a TLS key through pthread_key_create() system call. */
+    /*	Create a TLS key through pthread_key_create() system call. */
     static void tlsMakeKey()
     {
         (void)::pthread_key_create(&ThreadLocalLinuxImpl::m_tlsKey, ThreadLocalLinuxImpl::tlsDestructor);
     }
 
-    /*! @brief Destructor passed to pthread_key_create(). For any reason, thread exits before being able to call
-    * reset(), this will help to deallocate thread-local data object to avoid memory leak (because thread-local data
-    * maybe outside the manageable scope of thread cleanup handlers). See man 7 for more details */
+    /*	Destructor passed to pthread_key_create(). For any reason, thread exits before being able to call
+    * 	reset(), this will help to deallocate thread-local data object to avoid memory leak (because thread-local data
+    * 	maybe outside the manageable scope of thread cleanup handlers). See man 7 for more details */
     static void tlsDestructor(void* obj)
     {
         T* t = reinterpret_cast<T*>(obj);
@@ -126,19 +101,19 @@ private:
 
 }; // class ThreadLocalLinuxImpl
 
-template <class T, int instanceNum>
-pthread_key_t ThreadLocalLinuxImpl<T, instanceNum>::m_tlsKey = 0;
+template <class T>
+pthread_key_t ThreadLocalLinuxImpl<T>::m_tlsKey = 0;
 
-template <class T, int instanceNum>
-pthread_once_t ThreadLocalLinuxImpl<T, instanceNum>::m_tlsKeyOnce = PTHREAD_ONCE_INIT;
+template <class T>
+pthread_once_t ThreadLocalLinuxImpl<T>::m_tlsKeyOnce = PTHREAD_ONCE_INIT;
 
-template <class T, int instanceNum>
-bool ThreadLocalLinuxImpl<T, instanceNum>::m_isKeyCreated = false;
+template <class T>
+bool ThreadLocalLinuxImpl<T>::m_isKeyCreated = false;
 
 } // namespace implementation
 
-template <class T, int instanceNum = 1>
-using IThreadLocal = implementation::ThreadLocalLinuxImpl<T, instanceNum>;
+template <class T>
+using IThreadLocal = implementation::ThreadLocalLinuxImpl<T>;
 
 } // namespace V1
 
